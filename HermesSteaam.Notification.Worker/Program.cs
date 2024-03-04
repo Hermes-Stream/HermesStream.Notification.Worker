0b558a43eaee1e049c020227612dc5b049c8c7ba
julia.soares <julia.soares@vsoft.com.br>
2024-03-04 07:57:22 -0300

    Add project files.

---
+++ HermesSteaam.Notification.Worker/Program.cs
@@ -0,0 +1,10 @@
+using HermesStream.Notification.Worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
